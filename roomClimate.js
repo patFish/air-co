82893a6187dd3e946031689d8e37bc8a89a57633
patFish <fischer.pa7rick@gmail.com>
2021-05-03 14:18:42 +0200

    REFACTORE:delete unused path

--- roomClimate.js
+++ roomClimate.js
@@ -2,21 +2,18 @@ import { co2$ } from './co2Sensor'
 
 let roomLowLevel = new Map()
 
+const CO2THRESHOLD = 1200
+
 const roomLevel = co2$.subscribe(({ deviceId, payload }) => {
   const { value: co2 } = payload
-  let newCo2Level = co2 > 1200 ? 'high' : 'low'
+  let newCo2Level = co2 > CO2THRESHOLD ? 'high' : 'low'
+
   if (roomLowLevel.get(deviceId) === undefined) {
     roomLowLevel.set(deviceId, newCo2Level)
     console.log(`Sensor ${deviceId} level: ${newCo2Level}`)
   }
   const roomLevel = roomLowLevel.get(deviceId)
 
-  if (co2 > 1200 && roomLevel === 'low') {
-    newCo2Level = 'high'
-  }
-  if (co2 <= 1200 && roomLevel === 'high') {
-    newCo2Level = 'low'
-  }
   if (roomLevel != newCo2Level) {
     roomLowLevel.set(deviceId, newCo2Level)
     console.log(`Sensor ${deviceId} level: ${newCo2Level}`)
