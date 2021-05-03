65a54cfe418e6f5ab33f55b640bad73ebdbeb506
patFish <fischer.pa7rick@gmail.com>
2021-05-03 13:46:49 +0200

    FEAT: sensordata pipelines init

--- index.js
+++ index.js
@@ -1,27 +1,44 @@
+import { Subject } from 'rxjs'
 import { webSocket } from 'rxjs/webSocket'
-import { map } from 'rxjs/operators'
+import { map, filter } from 'rxjs/operators'
 
 global.WebSocket = require('ws') // <-- FIX ALL ERRORS
 const source = webSocket('wss://totally-fake-sensor-data.herokuapp.com')
 
-// const sensorLevelLow = false
-const subject = source.pipe(
+const sensorData$ = source.pipe(
   map((msg) => {
     const { deviceId, payload: rawPayload } = msg
-    console.log(deviceId, rawPayload)
     const type = parseInt(rawPayload.slice(0, 2), 10)
-    const value = rawPayload.slice(2)
-    console.log(type, value)
-    return msg
+    const value = parseInt(rawPayload.slice(2), 16)
+    return { deviceId, payload: { type, value } }
   })
 )
-// .multicast(() => new Rx.Subject())
 
-const battery$ = subject.subscribe((obj) => {
-  console.log(obj)
-  // console.log(obj.deviceId)
-  // console.log(obj.payload)
+const battery$ = sensorData$.pipe(filter((msg) => msg.payload.type === 2))
+
+battery$.subscribe((obj) => {
+  const { deviceId, payload } = obj
+  const { value } = payload
+  const batteryStatus = value / 1000
+  console.log(`Sensor ${deviceId} battery status: ${batteryStatus}V`)
 })
-// .filter((tweet) => tweet.user == '@angularfirebase')
 
-// .scan((totalScore, current) => totalScore + current)
+const co2$ = sensorData$.pipe(filter((msg) => msg.payload.type === 1))
+
+let roomLowLevel = new Map()
+roomLowLevel.set(1, false)
+roomLowLevel.set(2, false)
+
+const roomLevel = co2$.subscribe((obj) => {
+  const { deviceId, payload } = obj
+  const { value: newCo2Level } = payload
+  const isRoomLowLevel = roomLowLevel.get(deviceId)
+  if (newCo2Level > 1200 && isRoomLowLevel) {
+    roomLowLevel.set(deviceId, false)
+    console.log(`Sensor ${deviceId} level: ${newCo2Level}`)
+  }
+  if (newCo2Level <= 1200 && !isRoomLowLevel) {
+    roomLowLevel.set(deviceId, true)
+    console.log(`Sensor ${deviceId} level: ${newCo2Level}`)
+  }
+})
